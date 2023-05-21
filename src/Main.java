a08bd2fc7cc892951cfa24bb4f31bb614ed43843
MRSergoo <crstprod@gmail.com>
2023-05-21 22:55:24 +0300

    I have added deleting files more 5 month old

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,39 @@
 import java.io.File;
 import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import java.util.Date;
 
 public class Main {
     private static final String a = "C:";
-    private static SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+
 
 
     public static void main(String[] args)  {
 
        final File file = new File(a);
-        final File dirBackUP = new File("C:\\Users\\si.romanov\\Desktop\\BackUP");
+        final File dirBackUP = new File("C:\\Users\\Сергей\\Desktop\\BackUP");
         long totalSpace = file.getTotalSpace();
         long freeSpace = file.getFreeSpace();
         System.out.println("Total " + totalSpace + " Bytes ");
         System.out.println("Avaible "+ freeSpace + " Bytes");
         LocalDate timeNow = LocalDate.now();
+        System.out.println(timeNow.minusMonths(5));
+
         if (freeSpace < 226754275328l) {
             try {
                 for (File f : dirBackUP.listFiles()) {
                     for (File s : f.listFiles()) {
                         for (File c : s.listFiles()) {
-
-                            System.out.print(c.getName());
-                            System.out.print("  date create: ");
-                            System.out.println(c.lastModified());
-                            System.out.println(formatForDateNow.format(new Date(c.lastModified())));
+                            if (timeNow.minusMonths(5).isAfter((new Date(c.lastModified())).toInstant().atZone((ZoneId.systemDefault())).toLocalDate())) {
+                                c.delete();
 
 
+                            }
                         }
                     }
                 }
@@ -42,6 +45,5 @@ public class Main {
         }
 
 
-        System.out.println(timeNow.minusMonths(5));
     }
 }
