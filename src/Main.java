da7d2e72960c5a4d89b87b000d2a8613b63b1548
MRSergoo <crstprod@gmail.com>
2023-05-25 17:22:30 +0300

    Erase old version BackUp + delete files at FULL

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,55 @@
 import java.io.File;
 import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     private static final String a = "C:";
-    private static SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+    public static String aa = "C:\\Users\\si.romanov\\Desktop\\BackUP\\FULL";
 
+    public static void main(String[] args) {
 
-    public static void main(String[] args)  {
-
-       final File file = new File(a);
+        final File file = new File(a);
         final File dirBackUP = new File("C:\\Users\\si.romanov\\Desktop\\BackUP");
         long totalSpace = file.getTotalSpace();
         long freeSpace = file.getFreeSpace();
         System.out.println("Total " + totalSpace + " Bytes ");
-        System.out.println("Avaible "+ freeSpace + " Bytes");
+        System.out.println("Avaible " + freeSpace + " Bytes");
         LocalDate timeNow = LocalDate.now();
-        if (freeSpace < 226754275328l) {
-            try {
-                for (File f : dirBackUP.listFiles()) {
-                    for (File s : f.listFiles()) {
-                        for (File c : s.listFiles()) {
+        System.out.println(timeNow.minusDays(90));
+        int d = 5;
 
-                            System.out.print(c.getName());
-                            System.out.print("  date create: ");
-                            System.out.println(c.lastModified());
-                            System.out.println(formatForDateNow.format(new Date(c.lastModified())));
+//        if (freeSpace < 226754275328l) {
+//            try {
+//                for (File f : dirBackUP.listFiles()) {
+//                    for (File s : f.listFiles()) {
+//                        for (File c : s.listFiles()) {
+//                            if (timeNow.minusDays(90).isAfter((new Date(c.lastModified())).toInstant().atZone((ZoneId.systemDefault())).toLocalDate())) {
+//                                c.delete();
+//
+//
+//                            }
+//                        }
+//                    }
+//                }
+//            } catch (NullPointerException e) {
+//                System.out.println("any files not found at");
+//            }
+//        }
+        List<File> list = new ArrayList();
+        String aa = "C:\\Users\\si.romanov\\Desktop\\BackUP\\FULL";
+        File file1 = new File(aa);
 
+        for (File g : file1.listFiles(new Filter())) {
+            for (File k : g.listFiles()) {
 
-                        }
-                    }
-                }
             }
-            catch (NullPointerException e) {
-                System.out.println("any files not found at");
-            }
-
         }
-
-
-        System.out.println(timeNow.minusMonths(5));
     }
 }
